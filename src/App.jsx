05b1e53b1b68d01a1ea1d0e1327e3ba8e20ebe76
Aaron.S <steiger154@gmail.com>
2024-02-28 11:29:37 -0700

    Finished assignment

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className="first-react-group">
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul className="first-list">
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </div>
+    </>
+  )
+}
+
+export default App
